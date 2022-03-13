d64211c7830220100d86f3a040a2f4cf027ac4e0
jaemin-Yoo <iamjm29@gmail.com>
2022-03-13 13:43:44 +0900

    16236_아기상어

--- 16236.py
+++ 16236.py
@@ -1,57 +1,53 @@
+import sys
 from collections import deque
 
-def bfs(i, j):
-    visit = [[0] * n for i in range(n)]
-    visit[i][j] = 1
+def bfs(p):
+    global size, cnt, result
+    visited = [[0] * n for _ in range(n)]
+    dist = [[0] * n for _ in range(n)]
+    
     eat = []
-    dist = [[0] * n for i in range(n)]
+    visited[p[0]][p[1]] = 1
     q = deque()
-    q.append([i, j])
+    q.append(p)
     while q:
         x, y = q.popleft()
         for k in range(4):
             nx = x + dx[k]
             ny = y + dy[k]
-            if 0 <= nx < n and 0 <= ny < n and visit[nx][ny] == 0:
-                if s[nx][ny] <= s[i][j] or s[nx][ny] == 0:
-                    q.append([nx, ny])
-                    visit[nx][ny] = 1
-                    dist[nx][ny] = dist[x][y] + 1
-                if s[nx][ny] < s[i][j] and s[nx][ny] != 0:
-                    eat.append([nx, ny, dist[nx][ny]])
-    if not eat:
-        return -1, -1, -1
-    eat.sort(key = lambda x : (x[2], x[0], x[1]))
-    return eat[0][0], eat[0][1], eat[0][2]
-dx = [1, -1, 0, 0]
-dy = [0, 0, -1, 1]
+            if nx >= 0 and ny >= 0 and nx < n and ny < n and visited[nx][ny] == 0 and graph[nx][ny] <= size:
+                q.append((nx, ny))
+                visited[nx][ny] = 1
+                dist[nx][ny] = dist[x][y] + 1
+                
+                if graph[nx][ny] < size and graph[nx][ny] != 0:
+                    eat.append((nx, ny, dist[nx][ny]))
+                    
+    if eat != []:
+        eat = sorted(eat, key = lambda x: (x[2], x[0], x[1]))
+        
+        graph[eat[0][0]][eat[0][1]] = 0
+        cnt += 1
+        if size == cnt:
+            cnt = 0
+            size += 1
+        result += eat[0][2]
+        bfs((eat[0][0], eat[0][1]))
+                    
+input = sys.stdin.readline
 
 n = int(input())
-s = []
-test = [[0] * n for i in range(n)]
+graph = [list(map(int, input().strip().split())) for _ in range(n)]
 for i in range(n):
-    a = list(map(int, input().split()))
-    s.append(a)
     for j in range(n):
-        if a[j] == 9:
-            s[i][j] = 2
-            start = [i, j]
-exp = 0
+        if graph[i][j] == 9:
+            graph[i][j] = 0
+            start = (i, j)
+
+dx = [-1, 0, 0, 1]
+dy = [0, -1, 1, 0]
+size = 2
 cnt = 0
-while True:
-    i, j = start[0], start[1]
-    ex, ey, dist = bfs(i, j)
-    if ex == -1: break
-    s[ex][ey] = s[i][j]
-    s[i][j] = 0
-    start = [ex, ey]
-    exp += 1
-    if exp == s[ex][ey]:
-        exp = 0
-        s[ex][ey] += 1
-    cnt += dist
-    test[ex][ey] = 1
-    for m in test:
-        print(*m)
-    print()
-print(cnt)
+result = 0
+bfs(start)
+print(result)
---
+++ test.py
@@ -0,0 +1,7 @@
+for i in range(3):
+    a = 0
+    b = 1
+    print('hi')
+    if a == 0:
+        if b == 1:
+            break
