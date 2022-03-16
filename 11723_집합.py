3e7f174bb0d1b4cdf995910e910bf33ead0fef8f
jaemin-Yoo <iamjm29@gmail.com>
2022-03-16 17:27:31 +0900

    11723_집합

--- 11723_집합.py
+++ 11723_집합.py
@@ -3,28 +3,30 @@ input = sys.stdin.readline
 
 m = int(input())
 
-s = []
+s = set()
 for _ in range(m):
     a = input().strip().split()
     c = a[0]
     
+    if len(a) == 1:
+        if c == "all":
+            s = set([i for i in range(1, 21)])
+        elif c == "empty":
+            s = set()
+        continue
+    
+    v = int(a[1])
     if c == "add":
-        if a[1] not in s:
-            s.append(a[1])
+        s.add(v)
     elif c == "remove":
-        if a[1] in s:
-            s.remove(a[1])
+        s.discard(v)
     elif c == "check":
-        if a[1] in s:
+        if v in s:
             print(1)
         else:
             print(0)
     elif c == "toggle":
-        if a[1] in s:
-            s.remove(a[1])
+        if v in s:
+            s.discard(v)
         else:
-            s.append(a[1])
-    elif c == "all":
-        s = [str(i) for i in range(1, 21)]
-    elif c == "empty":
-        s = []
+            s.add(v)
