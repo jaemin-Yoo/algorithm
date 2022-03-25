4de466f1ab9d00f24bf805dd7a5af68b4180e02f
jaemin-Yoo <iamjm29@gmail.com>
2022-03-25 12:02:28 +0900

    11659_구간 합 구하기 4

--- test.py
+++ test.py
@@ -1,13 +1,2 @@
-n = int(input())
-d = [0] * (n + 1)
-d[0], d[1] = 0, 1
-
-for i in range(2, n + 1):
-    minValue = 1e9
-    j = 1
-    while (j ** 2) <= i:
-        minValue = min(minValue, d[i - (j ** 2)])
-        j += 1
-    d[i] = minValue + 1
-
-print(d[n])
+tmp = [0] + list(map(int, input().split()))
+print(tmp)
