c7594567fab03cd61416ab1a23ad9bc94db5a888
jaemin-Yoo <iamjm29@gmail.com>
2022-04-04 22:58:22 +0900

    14500_테트로미노

--- 14500_테트로미노.py
+++ 14500_테트로미노.py
@@ -1,23 +1,30 @@
 import sys
-from collections import deque
 input = sys.stdin.readline
 
-def bfs(start):
-    visited = [start]
-    q = deque()
-    q.append(start)
-    while q:
-        x, y = q.popleft()
-        for i in range(4):
-            a = x + dx[i]
-            b = y + dy[i]
-            if 0<=a<n and 0<=b<m and (a, b) not in visited and len(visited) < 4:
-                visited.append((a, b))
-                q.append((a, b))
+def dfs(start):
+    x, y = start
+    if len(arr) == 4:
+        s = 0
+        for a, b in arr:
+            s += graph[a][b]
+        result.append(s)
+        return
+    
+    for i in range(4):
+        a = x + dx[i]
+        b = y + dy[i]
+        if 0<=a<n and 0<=b<m and (a, b) not in arr:
+            if len(arr) == 2:
+                arr.append((a, b))
+                dfs(arr[1])
+                arr.pop()
+            arr.append((a, b))
+            dfs((a, b))
+            arr.pop()
 
 n, m = map(int, input().split())
 graph = []
-result_list = []
+result = []
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
@@ -26,4 +33,6 @@ for _ in range(n):
     
 for i in range(n):
     for j in range(m):
-        bfs((i, j))
+        arr = [(i, j)]
+        dfs((i, j))
+print(max(result))
--- 14502_연구소.py
+++ 14502_연구소.py
@@ -17,7 +17,7 @@ def makeWall(cnt):
 
 def bfs():
     q = copy.deepcopy(dq)
-    test = copy.deepcopy(graph)
+    g = copy.deepcopy(graph)
     visited = [[0 for _ in range(m)] for _ in range(n)]
     
     while q:
@@ -25,26 +25,23 @@ def bfs():
         for i in range(4):
             a = x + dx[i]
             b = y + dy[i]
-            if 0<=a<n and 0<=b<m and visited[a][b] == 0 and test[a][b] == 0:
+            if 0<=a<n and 0<=b<m and visited[a][b] == 0 and g[a][b] == 0:
                 visited[a][b] = 1
-                test[a][b] = 2
+                g[a][b] = 2
                 q.append((a, b))
     
     cnt = 0
     for i in range(n):
         for j in range(m):
-            if test[i][j] == 0:
+            if g[i][j] == 0:
                 cnt += 1
     cntList.append(cnt)
                 
-        
-
 n, m = map(int, input().split())
 graph = []
-
+cntList = []
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
-cntList = []
 
 for _ in range(n):
     graph.append(list(map(int, input().split())))
