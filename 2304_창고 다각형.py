75770f21f0cd0ce0097a8d8471a5249ddb3d4c9d
jaemin-Yoo <iamjm29@gmail.com>
2022-04-22 12:10:36 +0900

    2304_창고 다각형

---
+++ 2304_창고 다각형.py
@@ -0,0 +1,22 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+
+rec = [0 for _ in range(1001)]
+for _ in range(n):
+    l, h = map(int, input().split())
+    rec[l] = h
+max_idx = rec.index(max(rec))
+
+max_val = 0
+for i in range(max_idx):
+    max_val = max(max_val, rec[i])
+    rec[i] = max_val
+
+max_val = 0
+for i in range(1000, max_idx, -1):
+    max_val = max(max_val, rec[i])
+    rec[i] = max_val
+
+print(sum(rec))
