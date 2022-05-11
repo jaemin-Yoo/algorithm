46927928d11e0a116edde50fc7f187c24fbb37c9
jaemin-Yoo <iamjm29@gmail.com>
2022-05-11 12:43:13 +0900

    14501_퇴사(RE)

--- python/baekjoon/14501_퇴사(RE).py
+++ python/baekjoon/14501_퇴사(RE).py
@@ -3,13 +3,13 @@ input = sys.stdin.readline
 
 n = int(input())
 arr = []
-dp = [0 for i in range(n+1)]
+dp = [0 for _ in range(n+1)]
 for _ in range(n):
     arr.append(list(map(int, input().split())))
 
 for i in range(n-1, -1, -1):
     if arr[i][0] + i > n:
-        dp[i] = 0
+        dp[i] = dp[i+1]
     else:
         dp[i] = max(arr[i][1] + dp[i+arr[i][0]], dp[i+1])
 print(dp[0])
