84d285accfdcb1339f982fdaf9ecfab541e04549
jaemin-Yoo <iamjm29@gmail.com>
2022-05-12 12:03:33 +0900

    11057_오르막 수

---
+++ python/baekjoon/11057_오르막 수.py
@@ -0,0 +1,9 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+dp = [1 for _ in range(10)]
+for i in range(n-1):
+    for j in range(1, 10):
+        dp[j] += dp[j-1]
+print(sum(dp)%10007)
