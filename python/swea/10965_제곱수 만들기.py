a29185fa9941d7ae139db35a67e0094c786c75a2
jaemin-Yoo <iamjm29@naver.com>
2022-05-23 20:13:27 +0900

    test

--- python/swea/10965_제곱수 만들기.py
+++ python/swea/10965_제곱수 만들기.py
@@ -1,30 +1,34 @@
-arr = [True, True] + [False for _ in range(2, 10^7+1)]
-dec = []
-for i in range(2, a+1):
-    if i*i > a:
-        break
-    
+max_a = int((10**7)**0.5)
+arr = [True, True] + [False for _ in range(2, max_a+1)]
+for i in range(2, int(max_a**0.5)+1):
     if arr[i] == False:
-        dec.append(i)
-        for j in range(i, a+1, i):
+        for j in range(i*2, max_a+1, i):
             arr[j] = True
 
-for i in range(2, a+1):
+dec = []
+for i in range(2, len(arr)):        
     if arr[i] == False:
         dec.append(i)
-    
 
 t = int(input())
+res = []
 for idx in range(1, t+1):
     a = int(input())
-
     result = 1
+
     for v in dec:
         cnt = 0
         while a % v == 0:
-            a /= v
+            a //= v
             cnt += 1
         if cnt % 2 != 0:
             result *= v
+        if a == 1:
+            break
+    if a > 1:
+        result *= a
             
-    print("#{} {}".format(idx, result))
+    res.append(result)
+
+for idx in range(1, t+1):
+    print("#{} {}".format(idx, res[idx-1]))
