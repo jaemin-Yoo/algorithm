cc9e9a3666a8d70a531271cc327a3e4ed908e8e6
jaemin-Yoo <iamjm29@naver.com>
2022-05-26 01:33:22 +0900

    2814_최장 경로

---
+++ python/swea/d3/2814_최장 경로.py
@@ -0,0 +1,30 @@
+from collections import deque
+
+def dfs(node, dist):
+    global cnt
+    
+    visited[node] = 1
+    dist += 1
+    if cnt < dist:
+        cnt = dist
+    
+    for i in graph[node]:
+        if visited[i] == 0:
+            dfs(i, dist)
+
+t = int(input())
+for idx in range(1, t+1):
+    n, m = map(int, input().split())
+    
+    graph = [[] for _ in range(n+1)]
+    for _ in range(m):
+        x, y = map(int, input().split())
+        graph[x].append(y)
+        graph[y].append(x)
+    
+    cnt = 0
+    for i in range(1, n+1):
+        visited = [0 for _ in range(n+1)]
+        dfs(i, 0)
+        
+    print("#{} {}".format(idx, cnt))
