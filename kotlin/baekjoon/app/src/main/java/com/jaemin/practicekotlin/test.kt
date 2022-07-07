156f89b9b1e4a4822c360ab9584fe5c66cbe631f
jaemin-Yoo <iamjm29@gmail.com>
2022-07-07 12:22:58 +0900

    9093_단어 뒤집기

--- kotlin/baekjoon/app/src/main/java/com/jaemin/practicekotlin/test.kt
+++ kotlin/baekjoon/app/src/main/java/com/jaemin/practicekotlin/test.kt
@@ -1,5 +1,15 @@
 package com.jaemin.practicekotlin
 
-fun main() {
+import java.io.BufferedReader
+import java.io.InputStreamReader
 
+fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
+    val n = readLine()
+    var result = ""
+    for (i in n.indices) {
+        var a = Integer.toBinaryString(n[i]-'0')
+        if(a.length == 2 && i != 0) a = "0$a" else if (a.length == 1 && i != 0) a = "00$a"
+        result += a
+    }
+    println(result)
 }
