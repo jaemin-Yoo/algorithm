950b5376470d79de3346eaaf6f7530012f4f423d
jaemin-Yoo <iamjm29@gmail.com>
2022-07-09 15:03:00 +0900

    2445_별 찍기 - 8

--- kotlin/baekjoon/app/src/main/java/com/jaemin/practicekotlin/Mathematics/2445_별 찍기 - 8.kt
+++ kotlin/baekjoon/app/src/main/java/com/jaemin/practicekotlin/Mathematics/2445_별 찍기 - 8.kt
@@ -5,14 +5,6 @@ import java.io.InputStreamReader
 
 fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
     val n = readLine().toInt()
-
-    for (idx in 0 until 2*n) {
-        var i = idx
-        if (i >= n) {
-            i = 2 * (n-1) - idx
-        }
-
-        val row = "*".repeat(i+1) + " ".repeat(n - (i+1)) + " ".repeat(n-(i+1)) + "*".repeat(i+1)
-        println(row)
-    }
+    for (i in 1..n) println("*".repeat(i) + " ".repeat(2*n - (2*i)) + "*".repeat(i))
+    for (i in n-1 downTo 1) println("*".repeat(i) + " ".repeat(2*n - (2*i)) + "*".repeat(i))
 }
