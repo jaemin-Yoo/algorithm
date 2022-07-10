ce467f58d1c2ba578851e4265829007388ef0a68
jaemin-Yoo <iamjm29@naver.com>
2022-07-11 08:44:13 +0900

    폴더 구조 수정

--- kotlin/algorithm/app/src/main/java/com/jaemin/programmers/Level1/신고 결과 받기.kt
+++ kotlin/algorithm/app/src/main/java/com/jaemin/programmers/Level1/신고 결과 받기.kt
@@ -1,2 +1,8 @@
 package com.jaemin.programmers.Level1
 
+fun main() {
+    val triple = Triple("Hello", "Hi", 123)
+    println(triple.third)
+    println(triple.toString())
+    println(triple.toList())
+}
