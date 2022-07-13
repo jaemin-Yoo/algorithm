c49962f3fe0d80503c0312c6359814b0c7396857
jaemin-Yoo <iamjm29@gmail.com>
2022-07-13 09:30:23 +0900

    11655_ROT13

--- kotlin/algorithm/app/src/main/java/com/jaemin/boj/test.kt
+++ kotlin/algorithm/app/src/main/java/com/jaemin/boj/test.kt
@@ -4,11 +4,28 @@ import java.io.BufferedReader
 import java.io.InputStreamReader
 
 fun main() = with(BufferedReader(InputStreamReader(System.`in`))){
-    var sum = 0
-    repeat(5) {
-        var n =  readLine().toInt()
-        n = if (n > 40) n else 40
-        sum += n
+    val s = readLine()
+    for (c in s) {
+        when (c.code) {
+            in 65..90 -> {
+                val code = c.code + 13
+                if (code > 90) {
+                    print((code - 90 + 65 - 1).toChar())
+                } else {
+                    print(code.toChar())
+                }
+            }
+            in 97..122 -> {
+                val code = c.code + 13
+                if (code > 122) {
+                    print((code - 122 + 97 -1).toChar())
+                } else {
+                    print(code.toChar())
+                }
+            }
+            else -> {
+                print(c)
+            }
+        }
     }
-    println(sum / 5)
 }
