86903eeefe63251881635c608c0122b10602aa99
jaemin-Yoo <iamjm29@gmail.com>
2022-08-01 19:39:49 +0900

    9933_민균이의 비밀번호

--- kotlin/src/main/kotlin/boj/String/9933_민균이의 비밀번호.kt
+++ kotlin/src/main/kotlin/boj/String/9933_민균이의 비밀번호.kt
@@ -2,29 +2,12 @@ package boj.String
 
 fun main() {
     val n = readln().toInt()
-    val arr = ArrayList<String>()
-    repeat(n) {
-        arr.add(readln())
-    }
-
-    for (s in arr) {
-        var state = true
-        for (i in 0 until (s.length / 2)) {
-            if (s[i] != s[s.lastIndex - i]) {
-                state = false
-                break
-            }
-        }
-
-        if (state) {
-            print("${s.length} ")
-            println(s[s.length/2])
-            break
-        } else {
-            if (arr.contains(s.reversed())) {
-                print("${s.length} ")
-                println(s[s.length/2])
-                break
+    val arr = Array(n) { readln() }
+    run breaker@ {
+        arr.forEach {
+            if (it.reversed() in arr) {
+                println("${it.length} ${it[it.length / 2]}")
+                return@breaker
             }
         }
     }
