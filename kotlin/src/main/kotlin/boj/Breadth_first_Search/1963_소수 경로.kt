ccc73d48eca70841fe73be7737a8d5b07aed57fb
jaemin-Yoo <iamjm29@gmail.com>
2022-09-03 13:13:12 +0900

    1963_소수 경로

--- kotlin/src/main/kotlin/boj/Breadth_first_Search/1963_소수 경로.kt
+++ kotlin/src/main/kotlin/boj/Breadth_first_Search/1963_소수 경로.kt
@@ -1,12 +1,44 @@
 package boj.Breadth_first_Search
 
+import java.util.LinkedList
+import java.util.Queue
 import kotlin.math.sqrt
 
+lateinit var visited: MutableList<String>
 fun main() {
     val t = readln().toInt()
     repeat(t) {
         val (start, end) = readln().split(' ').map { it.toCharArray() }
+        visited = mutableListOf()
+        println(bfs(start, end))
     }
+}
+
+fun bfs(start: CharArray, end: CharArray): String {
+    val q: Queue<Pair<CharArray, Int>> = LinkedList()
+    q.add(start to 0)
+    visited.add(start.joinToString(""))
+    while (q.isNotEmpty()) {
+        val (n, depth) = q.poll()
+        if (n.contentEquals(end)) return depth.toString()
+
+        for (i in 0 until 4) {
+            for (j in 0..9) {
+                val strN = n.copyOf()
+                strN[i] = j.digitToChar()
+
+                val num = strN.joinToString("").toInt()
+                if (isPrimeNumber(num)
+                    && strN.joinToString("") !in visited
+                    && num >= 1000) {
+                    q.add(strN to depth + 1)
+                    visited.add(strN.joinToString(""))
+                }
+            }
+        }
+    }
+
+    return "Impossible"
 }
 
 fun isPrimeNumber(x: Int): Boolean {
