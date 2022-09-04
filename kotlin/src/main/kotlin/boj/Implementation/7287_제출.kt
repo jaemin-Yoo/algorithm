1868d17f316d7885cac315a25c12737b7c87f867
jaemin-Yoo <iamjm29@gmail.com>
2022-09-05 08:26:45 +0900

    7287_제출

---
+++ kotlin/src/main/kotlin/boj/Implementation/7287_제출.kt
@@ -0,0 +1,6 @@
+package boj.Implementation
+
+fun main() {
+    println(378)
+    println("iamjm29")
+}
--- kotlin/src/main/kotlin/boj/test.kt
+++ kotlin/src/main/kotlin/boj/test.kt
@@ -1,49 +1,10 @@
 package com.jaemin.practicekotlin
 
-import java.util.*
-
-class Test {
-    data class Bucket(
-        val a: Int,
-        val b: Int,
-        val c: Int
-    )
-
-    val visited = mutableListOf<Bucket>()
-    var maxA = 0
-    var maxB = 0
-    var maxC = 0
-    fun main() {
-        val (_maxA, _maxB, _maxC) = readln().split(' ').map { it.toInt() }
-        maxA = _maxA
-        maxB = _maxB
-        maxC = _maxC
-        BFS(Bucket(0, 0, maxC))
+fun main() {
+    val t: Int? = null
+    val a = t ?: t.let {
+        println(it)
+        3
     }
-
-    fun BFS(start: Bucket) {
-        val q: Queue<Bucket> = LinkedList()
-        q.add(start)
-        visited.add(start)
-        while (q.isNotEmpty()) {
-            val (x, y, z) = q.poll()
-            var a = x
-            var b = y
-            var c = z
-            if (x != 0) {
-                if (x + y > maxB) {
-                    a = x + y - maxB
-                    b = maxB
-                } else {
-                    a = 0
-                    b = x + y
-                }
-                q.add(Bucket(a, b, c))
-            }
-        }
-    }
-
-    fun moveWater(a: Int, b: Int, c: Int, q: Queue<Bucket>) {
-
-    }
+    println(a)
 }
