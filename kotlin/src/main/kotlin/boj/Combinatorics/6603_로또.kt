0514bdd99c7678bfb89fa23973c41b56c4a402d9
jaemin-Yoo <iamjm29@gmail.com>
2022-09-11 16:57:41 +0900

    6603_로또

--- kotlin/src/main/kotlin/boj/Combinatorics/6603_로또.kt
+++ kotlin/src/main/kotlin/boj/Combinatorics/6603_로또.kt
@@ -1,35 +1,29 @@
 package boj.Combinatorics
 
-var n = 0
-val result = mutableListOf<String>()
-lateinit var a: MutableList<Int>
-fun main() {
-    while (true) {
-        a = readln().split(' ').map { it.toInt() }.toMutableList()
-        n = a[0]
-        a.removeFirst()
-        combine(mutableListOf())
-
-        result.forEach {
-            println(it)
+class 로또6603 {
+    var n = 0
+    lateinit var a: MutableList<Int>
+    fun main() {
+        while (true) {
+            a = readln().split(' ').map { it.toInt() }.toMutableList()
+            if (a[0] == 0) break
+            n = a[0]
+            a.removeFirst()
+            combine(mutableListOf(), 0)
+            println()
         }
     }
-}
 
-fun combine(arr: MutableList<Int>) {
-    if (n == arr.size) {
-        val s = arr.joinToString(" ")
-        if (!result.contains(s)) {
-            result.add(s)
+    fun combine(arr: MutableList<Int>, start: Int) {
+        if (arr.size == 6) {
+            val s = arr.joinToString(" ")
+            println(s)
+            return
         }
 
-        return
-    }
-
-    for (i in a) {
-        if (i !in arr) {
-            arr.add(i)
-            combine(arr)
+        for (i in start until a.size) {
+            arr.add(a[i])
+            combine(arr, i + 1)
             arr.removeLast()
         }
     }
