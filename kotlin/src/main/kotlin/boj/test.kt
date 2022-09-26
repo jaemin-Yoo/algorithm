37210828d444276d3afa7473bc21959c07063700
jaemin-Yoo <iamjm29@gmail.com>
2022-09-26 21:19:27 +0900

    1094_막대기

--- kotlin/src/main/kotlin/boj/test.kt
+++ kotlin/src/main/kotlin/boj/test.kt
@@ -1,5 +1,13 @@
 package com.jaemin.practicekotlin
 
+import java.util.LinkedList
+import java.util.Queue
+
 fun main() {
+    println(isExpTwo(4))
+    println(isExpTwo(16))
+    println(isExpTwo(2))
 
 }
+
+fun isExpTwo(n: Int) = n and (n - 1) == 0
