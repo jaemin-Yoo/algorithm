4c178e306c1fe40639766c2dcfce59040879a99b
jaemin-Yoo <iamjm29@gmail.com>
2022-09-30 10:35:05 +0900

    1158_요세푸스 문제

--- kotlin/src/main/kotlin/boj/test.kt
+++ kotlin/src/main/kotlin/boj/test.kt
@@ -1,7 +1,15 @@
 package com.jaemin.practicekotlin
 
 fun main() {
-    println(".  .   .\n" +
-            "|  | _ | _. _ ._ _  _\n" +
-            "|/\\|(/.|(_.(_)[ | )(/.")
+    val TEN_MILLION = 10000000
+    readln()
+    val cards = Array(TEN_MILLION * 2 + 1) { 0 }
+    readln().split(" ").map {
+        cards[TEN_MILLION + it.toInt()] = 1
+    }
+    readln()
+    val result = readln().split(" ").map {
+        cards[TEN_MILLION + it.toInt()]
+    }
+    println(result.joinToString(" "))
 }
