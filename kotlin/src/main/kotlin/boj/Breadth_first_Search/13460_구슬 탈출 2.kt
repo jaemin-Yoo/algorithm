20ffd10322c35491c3d08da344c7ca27fbd55cc9
jaemin-Yoo <iamjm29@gmail.com>
2022-10-02 17:31:38 +0900

    13460_구슬 탈출 2

--- kotlin/src/main/kotlin/boj/Breadth_first_Search/13460_구슬 탈출 2.kt
+++ kotlin/src/main/kotlin/boj/Breadth_first_Search/13460_구슬 탈출 2.kt
@@ -1,135 +1,90 @@
-package boj.Breadth_first_Search
+package boj.Breadth_first_Search.구슬탈출2
 
-import java.util.LinkedList
-import java.util.Queue
+import java.util.*
 
 val graph = mutableListOf<MutableList<Char>>()
-val visited by lazy { Array(n) { Array(m) { 0 } } }
+lateinit var visited: Array<Array<Array<Array<Int>>>>
 val dx = listOf(-1, 1, 0, 0)
 val dy = listOf(0, 0, -1, 1)
-var n = 0
-var m = 0
 fun main() {
-    val (_n, _m) = readln().split(" ").map { it.toInt() }
-    n = _n
-    m = _m
-    var redX = 0
-    var redY = 0
-    var blueX = 0
-    var blueY = 0
-    repeat(n) {
+    val (n ,m) = readln().split(" ").map { it.toInt() }
+    var (rx, ry, bx, by) = listOf(-1, -1, -1, -1)
+    visited = Array(n) { Array(m) { Array(n) { Array(m) { 0 } } } }
+    repeat(n) { x ->
         val row = readln().toMutableList()
-        graph.add(row)
-        val idx = row.indexOf('R')
-        if (idx != -1) {
-            redX = it
-            redY = idx
+        val redY = row.indexOf('R')
+        val blueY = row.indexOf('B')
+
+        if (redY != -1) {
+            rx = x
+            ry = redY
         }
 
-        val blueIdx = row.indexOf('B')
-        if (blueIdx != -1) {
-            blueX = it
-            blueY = blueIdx
+        if (blueY != -1) {
+            bx = x
+            by = blueY
         }
+
+        graph.add(row)
     }
-    val dist = bfs(Triple(redX, redY, 1), BluePoint(blueX, blueY, blueX, blueY))
-    val result = if (dist > 10) {
-        -1
-    } else {
-        dist
-    }
+    visited[rx][ry][bx][by] = 1
+    val result = bfs(Node(rx, ry, bx, by, 1))
     println(result)
 }
 
-fun bfs(redStart: Triple<Int, Int, Int>, blueStart: BluePoint): Int {
-    val redQ: Queue<Triple<Int, Int, Int>> = LinkedList()
-    val blueQ: Queue<BluePoint> = LinkedList()
-    redQ.add(redStart)
-    blueQ.add(blueStart)
-    visited[redStart.first][redStart.second] = 1
-    while (redQ.isNotEmpty()) {
-        val (x, y, dist) = redQ.poll()
-        val (bx, by, prevX, prevY) = blueQ.poll()
-
+fun bfs(node: Node): Int {
+    val q: Queue<Node> = LinkedList()
+    q.add(node)
+    while (q.isNotEmpty()) {
+        val (rx, ry, bx, by, depth) = q.poll()
+        if (depth > 10) {
+            return -1
+        }
         for (i in 0 until 4) {
-            val redDes = moveRed(x, y, i)
-            val blueDes = moveBlue(bx, by, i)
-            var redPoint = Triple(redDes.first, redDes.second, dist + 1)
-            var bluePoint = BluePoint(blueDes.first, blueDes.second, bx, by)
+            var (ra, rb, rCnt) = move(rx, ry, dx[i], dy[i])
+            var (ba, bb, bCnt) = move(bx, by, dx[i], dy[i])
+            if (graph[ba][bb] != '#' && graph[ba][bb] != 'O') {
+                if (graph[ra][rb] == 'O') { // RED가 빠져나갔을 때
+                    return depth
+                }
 
-            if (blueDes == Triple(0, 0, 0)) {
-                continue
-            }
+                if (ra == ba && rb == bb) { // RED, BLUE 위치가 같은 경우
+                    if (rCnt > bCnt) {
+                        ra -= dx[i]
+                        rb -= dy[i]
+                    } else {
+                        ba -= dx[i]
+                        bb -= dy[i]
+                    }
+                }
 
-            if (redDes == blueDes) {
-                if (redDes.third > blueDes.third) {
-                    val a = redDes.first - dx[i]
-                    val b = redDes.second - dy[i]
-                    redPoint = Triple(a, b, dist + 1)
-                    visited[a][b] = 1
-                } else {
-                    val a = redDes.first
-                    val b = redDes.second
-                    bluePoint = BluePoint(blueDes.first - dx[i], blueDes.second - dy[i], bx, by)
-                    visited[a][b] = 1
+                if (visited[ra][rb][ba][bb] == 0) {
+                    visited[ra][rb][ba][bb] = 1
+                    q.add(Node(ra, rb, ba, bb, depth + 1))
                 }
             }
-
-            println("$redDes $blueDes $bluePoint")
-
-            if (redDes == Triple(0, 0, 0)) {
-                return dist
-            }
-
-            redQ.add(redPoint)
-            blueQ.add(bluePoint)
         }
     }
 
     return -1
 }
 
-fun moveRed(x: Int, y: Int, i: Int): Triple<Int, Int, Int> {
-    var a = x + dx[i]
-    var b = y + dy[i]
+fun move(x: Int, y: Int, dx: Int, dy: Int): Triple<Int, Int, Int> {
     var cnt = 0
-    var des = Triple(x, y, cnt)
-    while (true) {
+    var a = x
+    var b = y
+    while (graph[a+dx][b+dy] != '#' && graph[a][b] != 'O') {
+        a += dx
+        b += dy
         cnt += 1
-        if ((graph[a][b] == '.' || graph[a][b] == 'B') && visited[a][b] == 0) {
-            des = Triple(a, b, cnt)
-            a += dx[i]
-            b += dy[i]
-        } else if (graph[a][b] == 'O') {
-            return Triple(0, 0, 0)
-        } else {
-            return des
-        }
     }
+    return Triple(a, b, cnt)
 }
 
-fun moveBlue(x: Int, y: Int, i: Int): Triple<Int, Int, Int> {
-    var a = x + dx[i]
-    var b = y + dy[i]
-    var cnt = 0
-    var des = Triple(x, y, cnt)
-    while (true) {
-        cnt += 1
-        if (graph[a][b] == '.' || graph[a][b] == 'R' || graph[a][b] == 'B') {
-            des = Triple(a, b, cnt)
-            a += dx[i]
-            b += dy[i]
-        } else if (graph[a][b] == 'O') {
-            return Triple(0, 0, 0)
-        } else {
-            return des
-        }
-    }
-}
-
-data class BluePoint(
-    val curX: Int,
-    val curY: Int,
-    val prevX: Int,
-    val prevY: Int
+data class Node(
+    val rx: Int,
+    val ry: Int,
+    val bx: Int,
+    val by: Int,
+    val depth: Int
 )
