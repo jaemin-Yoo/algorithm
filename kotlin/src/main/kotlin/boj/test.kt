bc7b77fb362e1fd8a301d6591883879afbd08b75
jaemin-Yoo <iamjm29@naver.com>
2022-10-10 18:07:32 +0900

    2075_N번째 큰 수

--- kotlin/src/main/kotlin/boj/test.kt
+++ kotlin/src/main/kotlin/boj/test.kt
@@ -1,60 +1,18 @@
 package com.jaemin.practicekotlin
 
-val cbnList = mutableListOf<List<Int>>()
 fun main() {
-    val s = readln()
-    val count = s.count { it == '(' }
-    for (i in 1..count) {
-        combine(mutableListOf(), i, count, 1)
+    val n = readln().toInt()
+    val graph = mutableListOf<List<Int>>()
+    repeat(n) {
+        graph.add(readln().split(" ").map { it.toInt() })
     }
-
-    val result = mutableListOf<String>()
-    for (arr in cbnList) {
-        var openCnt = 1
-        var closeCnt = count
-        val exp = s.map {
-            when (it) {
-                '(' -> {
-                    val v = if (openCnt !in arr) {
-                        it
-                    } else {
-                        ""
-                    }
-                    openCnt += 1
-                    v
-                }
-                ')' -> {
-                    val v = if (closeCnt !in arr) {
-                        it
-                    } else {
-                        ""
-                    }
-                    closeCnt -= 1
-                    v
-                }
-                else -> it
-            }
-        }.joinToString("")
-        result.add(exp)
+    var x = n * n - n
+    var y = 1
+    while (x !in 1..n) {
+        x -= n * y
+        y += 1
     }
-
-    result.sort()
-    result.forEach {
-        println(it)
-    }
-}
-
-fun combine(arr: MutableList<Int>, n: Int, count: Int, start: Int) {
-    if (arr.size == n) {
-        cbnList.add(arr.toList())
-        return
-    }
-
-    for (i in start..count) {
-        if (i !in arr) {
-            arr.add(i)
-            combine(arr, n, count, i + 1)
-            arr.removeLast()
-        }
-    }
+    val v = graph[0].sorted()[x - 1]
+    val i = graph[0].indexOf(v)
+    println(graph[i][y])
 }
