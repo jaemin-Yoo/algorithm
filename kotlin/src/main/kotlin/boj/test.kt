a06a7200006bcf78b2e6405aced19280701d04e8
jaemin-Yoo <iamjm29@gmail.com>
2022-10-16 20:17:15 +0900

    20053_최소, 최대 2

--- kotlin/src/main/kotlin/boj/test.kt
+++ kotlin/src/main/kotlin/boj/test.kt
@@ -1,31 +1,47 @@
 package com.jaemin.practicekotlin
 
-import java.util.PriorityQueue
+import java.util.*
 
+val dx = listOf(-1, 1, 0, 0, -1, 1, -1, 1)
+val dy = listOf(0, 0, -1, 1, -1, 1, 1, -1)
+var n = 0
+var cnt = 0
 fun main() {
-    val t = readln().toInt()
-    repeat(t) {
-        val n = readln().toInt()
-        val list = readln().split(" ").map { it.toInt() }
+    n = readln().toInt()
+    val visited = MutableList(n) { MutableList(3) { 0 } }
+    bfs(Point(0, 0, 1, visited))
+    println(cnt % 1000000000)
+}
 
-        val pq = PriorityQueue<Int>()
-        val result = mutableListOf<Int>()
-        for (i in list.indices) {
-            pq.add(list[i])
-            if (i % 2 == 0) {
-                val center = if (i == 0) {
-                    pq.poll()
-                } else {
-                    pq.poll()
-                    val center = pq.poll()
-                    pq.poll()
-                    center
-                }
-                result.add(center)
-                pq.add(center)
+fun bfs(start: Point) {
+    val q: Queue<Point> = LinkedList()
+    start.visited[start.x][start.y] = 1
+    q.add(start)
+    while (q.isNotEmpty()) {
+        val (x, y, c, v) = q.poll()
+        if (x == n - 1 && y == 2) {
+            if (c == n * 3) cnt += 1
+        }
+        for (i in 0 until 8) {
+            val a = x + dx[i]
+            val b = y + dy[i]
+            if (a in 0 until n
+                && b in 0 until 3
+                && v[a][b] == 0) {
+                val temp = v.map {
+                    it.toMutableList()
+                }.toMutableList()
+                temp[a][b] = 1
+
+                q.add(Point(a, b, c + 1, temp))
             }
         }
-        println(n / 2 + 1)
-        println(result.joinToString(" "))
     }
 }
+
+data class Point(
+    val x: Int,
+    val y: Int,
+    val cnt: Int,
+    val visited: MutableList<MutableList<Int>>
+)
