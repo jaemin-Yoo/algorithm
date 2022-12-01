e58428d32b93650dcda5be2fb092ef2ef44a2639
jaemin-Yoo <iamjm29@gmail.com>
2022-12-01 21:09:04 +0900

    10886

--- kotlin/src/main/kotlin/boj/test.kt
+++ kotlin/src/main/kotlin/boj/test.kt
@@ -1,5 +1,19 @@
 package com.jaemin.practicekotlin
 
 fun main() {
+    val n = readln().toInt()
+    val list = mutableListOf<Int>()
+    repeat(n) {
+        val a = readln().toInt()
+        list.add(a)
+    }
 
+    val cute = list.count { it == 1 }
+    val notCute = list.count { it == 0 }
+    val result = if (cute > notCute) {
+        "Junhee is cute!"
+    } else {
+        "Junhee is not cute!"
+    }
+    println(result)
 }
