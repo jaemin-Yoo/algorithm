8eb757c41e0130d3b46562861c51a555d717cf2b
jaemin-Yoo <iamjm29@gmail.com>
2022-12-02 20:30:09 +0900

    25305

--- kotlin/src/main/kotlin/boj/test.kt
+++ kotlin/src/main/kotlin/boj/test.kt
@@ -1,19 +1,7 @@
 package com.jaemin.practicekotlin
 
 fun main() {
-    val n = readln().toInt()
-    val list = mutableListOf<Int>()
-    repeat(n) {
-        val a = readln().toInt()
-        list.add(a)
-    }
-
-    val cute = list.count { it == 1 }
-    val notCute = list.count { it == 0 }
-    val result = if (cute > notCute) {
-        "Junhee is cute!"
-    } else {
-        "Junhee is not cute!"
-    }
-    println(result)
+    val (a, b) = readln().split(" ").map { it.toInt() }
+    val list = readln().split(" ").map { it.toInt() }.sortedDescending()
+    println(list[b - 1])
 }
