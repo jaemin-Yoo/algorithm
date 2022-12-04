2e451f48bf196cf53255dd3abf9581753a493c69
jaemin-Yoo <iamjm29@gmail.com>
2022-12-04 20:30:01 +0900

    15686_치킨배달

--- kotlin/src/main/kotlin/boj/Bruteforcing/15686_치킨배달.kt
+++ kotlin/src/main/kotlin/boj/Bruteforcing/15686_치킨배달.kt
@@ -1,46 +1,62 @@
 package boj.Bruteforcing.치킨배달15686
 
-val graph = mutableListOf<MutableList<Int>>()
-val selected = mutableListOf<MutableList<Pair<Int, Int>>>()
-val chicken = mutableListOf<Pair<Int, Int>>()
+import kotlin.math.abs
+import kotlin.math.min
+
+val graph = mutableListOf<List<Int>>()
+val selected = mutableListOf<List<Node>>()
+val home = mutableListOf<Node>()
+val chicken = mutableListOf<Node>()
 var n = 0
 var m = 0
+
+data class Node(
+    val x: Int,
+    val y: Int
+)
 
 fun main() = with(System.`in`.bufferedReader()) {
     val (_n, _m) = readLine().split(" ").map { it.toInt() }
     n = _n
     m = _m
     repeat(n) {
-        graph.add(readLine().split(" ").map { it.toInt() }.toMutableList())
+        graph.add(readLine().split(" ").map { it.toInt() })
     }
 
-    val home = mutableListOf<Pair<Int, Int>>()
     for (i in 0 until n) {
         for (j in 0 until n) {
             when (graph[i][j]) {
-                1 -> home.add(i to j)
-                2 -> chicken.add(i to j)
+                1 -> home.add(Node(i, j))
+                2 -> chicken.add(Node(i, j))
             }
         }
     }
 
-    dfs(0, 0, mutableListOf())
-    selected.forEach {
-        println(it.joinToString(" "))
+    dfs(0, 0, Array(m) { Node(-1, -1) })
+    var mnResult = Int.MAX_VALUE
+    selected.forEach { chickens ->
+        var s = 0
+        home.forEach { (x, y) ->
+            val mn = chickens.minOf { (a, b) ->
+                abs(a - x) + abs(b - y)
+            }
+            s += mn
+        }
+        mnResult = min(mnResult, s)
     }
+    println(mnResult)
 
     close()
 }
 
-fun dfs(start: Int, depth: Int, ans: MutableList<Pair<Int, Int>>) {
+fun dfs(start: Int, depth: Int, ans: Array<Node>) {
     if (depth == m) {
-        selected.add(ans.toMutableList())
+        selected.add(ans.toList())
         return
     }
 
     for (i in start until chicken.size) {
-        ans.add(chicken[i])
+        ans[depth] = chicken[i]
         dfs(i + 1, depth + 1, ans)
-        ans.removeLast()
     }
 }
