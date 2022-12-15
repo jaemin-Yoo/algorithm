a381c4c1fe8eefcbdf73b8547b04cfad8244232d
jaemin-Yoo <iamjm29@gmail.com>
2022-12-15 18:25:49 +0900

    14659_한조서열정리하고옴ㅋㅋ

--- kotlin/src/main/kotlin/boj/test.kt
+++ kotlin/src/main/kotlin/boj/test.kt
@@ -1,8 +1,22 @@
 package com.jaemin.practicekotlin
 
-import kotlin.math.abs
-
+import kotlin.math.max
 fun main() = with(System.`in`.bufferedReader()) {
-    val (n, m) = readLine().split(" ").map { it.toLong() }
-    println(abs(n - m))
+    val n = readLine().toInt()
+    val list = readLine().split(" ").map { it.toInt() }
+
+    var result = 0
+    for (i in 0 until n) {
+        var cnt = 0
+        for (j in i + 1 until n) {
+            if (list[i] > list[j]) {
+                cnt += 1
+            } else {
+                result = max(result, cnt)
+                break
+            }
+        }
+        result = max(result, cnt)
+    }
+    println(result)
 }
