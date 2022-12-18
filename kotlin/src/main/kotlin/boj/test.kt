a77527533a393c17b486ff24f9aefe1b5793b986
jaemin-Yoo <iamjm29@gmail.com>
2022-12-18 16:22:56 +0900

    2460_지능형 기차 2

--- kotlin/src/main/kotlin/boj/test.kt
+++ kotlin/src/main/kotlin/boj/test.kt
@@ -1,22 +1,15 @@
 package com.jaemin.practicekotlin
 
 import kotlin.math.max
-fun main() = with(System.`in`.bufferedReader()) {
-    val n = readLine().toInt()
-    val list = readLine().split(" ").map { it.toInt() }
 
+fun main() {
+    var max = 0
     var result = 0
-    for (i in 0 until n) {
-        var cnt = 0
-        for (j in i + 1 until n) {
-            if (list[i] > list[j]) {
-                cnt += 1
-            } else {
-                result = max(result, cnt)
-                break
-            }
-        }
-        result = max(result, cnt)
+    repeat(10) {
+        val (a, b) = readln().split(" ").map{ it.toInt() }
+        result -= a
+        result += b
+        max = max(max, result)
     }
-    println(result)
+    println(max)
 }
