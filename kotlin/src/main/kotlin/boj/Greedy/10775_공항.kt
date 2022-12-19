347ecf376567d59987dc4be1d8f98d4eeab84dc0
jaemin-Yoo <iamjm29@gmail.com>
2022-12-19 16:06:59 +0900

    9653_스타워즈로고

---
+++ kotlin/src/main/kotlin/boj/Greedy/10775_공항.kt
@@ -0,0 +1,17 @@
+package boj.Greedy
+
+fun main() = with(System.`in`.bufferedReader()) {
+    val g = readLine().toInt()
+    val p = readLine().toInt()
+    repeat(p) {
+        val i = readLine().toInt(0)
+    }
+
+    close()
+}
+
+/*
+1 2 3 4
+2 2 3
+
+ */
--- kotlin/src/main/kotlin/boj/test.kt
+++ kotlin/src/main/kotlin/boj/test.kt
@@ -3,13 +3,16 @@ package com.jaemin.practicekotlin
 import kotlin.math.max
 
 fun main() {
-    var max = 0
-    var result = 0
-    repeat(10) {
-        val (a, b) = readln().split(" ").map{ it.toInt() }
-        result -= a
-        result += b
-        max = max(max, result)
-    }
-    println(max)
+    println("""
+    8888888888  888    88888
+   88     88   88 88   88  88
+    8888  88  88   88  88888
+       88 88 888888888 88   88
+88888888  88 88     88 88    888888
+
+88  88  88   888    88888    888888
+88  88  88  88 88   88  88  88
+88 8888 88 88   88  88888    8888
+ 888  888 888888888 88  88      88
+  88  88  88     88 88   88888888""".trimIndent())
 }
