2d3cb190b3cd0548cd8d40f0cdf8b92e590020ca
jaemin-Yoo <iamjm29@gmail.com>
2023-05-27 14:46:30 +0900

    Hacker rank

---
+++ kotlin/src/main/kotlin/boj/hackerrank/problem3.kt
@@ -0,0 +1,36 @@
+package boj.hackerrank
+
+fun main() {
+    val text = "abcd"
+    val prefixString = "eeab"
+    val suffixString = "aeee"
+    val result = calculateScore(text, prefixString, suffixString)
+    println(result)
+}
+
+fun calculateScore(text: String, prefixString: String, suffixString: String): String {
+    var prefixText = ""
+    for (i in 1 .. prefixString.length) {
+        val s = prefixString.substring(prefixString.length - i, prefixString.length)
+        if (s in text) {
+            prefixText = s // 한글자 확인 (사전순)
+        }
+    }
+
+    var suffixText = ""
+    for (i in 1 .. suffixString.length) {
+        val s = suffixString.substring(0, i)
+        if (s in text) {
+            suffixText = s // 한글자 확인 (사전순)
+        }
+    }
+
+    val start = text.indexOf(prefixText)
+    val end = text.indexOf(suffixText)
+
+    return if (start > end) {
+        listOf(prefixText, suffixText).sorted()[0]
+    } else {
+        text.substring(start, end + suffixText.length)
+    }
+}
