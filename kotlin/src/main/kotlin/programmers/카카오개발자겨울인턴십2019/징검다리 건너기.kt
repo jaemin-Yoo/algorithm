b8061907ded8e2213faa8c4deac34efeaffcf7d6
jaemin-Yoo <iamjm29@gmail.com>
2023-11-18 20:21:44 +0900

    징검다리 건너기

---
+++ kotlin/src/main/kotlin/programmers/카카오개발자겨울인턴십2019/징검다리 건너기.kt
@@ -0,0 +1,35 @@
+package programmers.카카오개발자겨울인턴십2019.징검다리건너기
+
+// 못품
+
+class Solution {
+    fun solution(stones: IntArray, k: Int): Int {
+        var result = 0
+        var (left, right) = 0 to stones.maxOf { it }
+
+        while (left <= right) {
+            val mid = (left + right) / 2
+            var cnt = 0
+            for (i in stones.indices) {
+                val diff = stones[i] - mid
+                if (diff <= 0) {
+                    cnt += 1
+                } else {
+                    cnt = 0
+                }
+
+                if (cnt >= k) {
+                    result = mid
+                    right = mid - 1
+                    break
+                }
+            }
+
+            if (cnt < k) {
+                left = mid + 1
+            }
+        }
+
+        return result
+    }
+}
