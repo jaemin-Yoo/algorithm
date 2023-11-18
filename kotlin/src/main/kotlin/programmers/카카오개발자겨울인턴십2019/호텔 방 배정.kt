870af47da1726488e9ed936adbef9557faf0e041
jaemin-Yoo <iamjm29@gmail.com>
2023-11-18 20:23:32 +0900

    호텔 방 배정

---
+++ kotlin/src/main/kotlin/programmers/카카오개발자겨울인턴십2019/호텔 방 배정.kt
@@ -0,0 +1,27 @@
+package programmers.카카오개발자겨울인턴십2019.호텔방배정
+
+// 못품
+
+class Solution {
+    fun solution(k: Long, room_number: LongArray): LongArray {
+        val hashMap = HashMap<Long, Long>()
+        val result = LongArray(room_number.size)
+        for (i in room_number.indices) {
+            val roomNumber = room_number[i]
+            result[i] = findEmptyRoom(hashMap, roomNumber)
+        }
+
+        return result
+    }
+
+    private fun findEmptyRoom(hashMap: HashMap<Long, Long>, roomNumber: Long): Long {
+        val number = if (hashMap.containsKey(roomNumber)) {
+            findEmptyRoom(hashMap, hashMap[roomNumber]!!)
+        } else {
+            roomNumber
+        }
+
+        hashMap[roomNumber] = number + 1
+        return number
+    }
+}
