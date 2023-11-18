ccc1e7194c3e31a636eaa073ddc6316573fc0822
jaemin-Yoo <iamjm29@gmail.com>
2023-11-18 16:16:38 +0900

    튜플

---
+++ kotlin/src/main/kotlin/programmers/카카오개발자겨울인턴십2019/튜플.kt
@@ -0,0 +1,29 @@
+package programmers.카카오개발자겨울인턴십2019.튜플
+
+// 30분
+
+class Solution {
+    fun solution(s: String): IntArray {
+        val list = convertToList(s)
+        val result = mutableListOf<Int>()
+
+        for (l in list) {
+            for (n in l) {
+                if (n !in result) {
+                    result.add(n)
+                }
+            }
+        }
+
+        return result.toIntArray()
+    }
+
+    private fun convertToList(s: String): List<List<Int>> {
+        val list = s.trim('{', '}').split("},{").map {
+            it.trim('{', '}').split(",").map { c -> c.toInt() }
+        }.toMutableList()
+        list.sortBy { it.size }
+
+        return list
+    }
+}
