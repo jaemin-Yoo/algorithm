6aef924da59a8e442cc771ea7154a0e51b4e54bd
jaemin-Yoo <iamjm29@gmail.com>
2023-11-18 16:16:47 +0900

    크레인 인형뽑기 게임

---
+++ kotlin/src/main/kotlin/programmers/카카오개발자겨울인턴십2019/크레인 인형뽑기 게임.kt
@@ -0,0 +1,30 @@
+package programmers.카카오개발자겨울인턴십2019.크레인인형뽑기게임
+
+// 26분
+
+import java.util.Stack
+
+class Solution {
+    fun solution(board: Array<IntArray>, moves: IntArray): Int {
+        var result = 0
+        val basket = Stack<Int>()
+
+        moves.forEach { j ->
+            for (i in 0 until board.size) {
+                val doll = board[i][j - 1]
+                if (doll > 0) {
+                    board[i][j - 1] = 0
+                    if (basket.isNotEmpty() && basket.peek() == doll) {
+                        basket.pop()
+                        result += 2
+                    } else {
+                        basket.add(doll)
+                    }
+                    break
+                }
+            }
+        }
+
+        return result
+    }
+}
