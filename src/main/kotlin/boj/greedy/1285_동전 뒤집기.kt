a1dd2c85c7c6910fce370ab68f5376d7cfb58db6
jaemin-Yoo <iamjm29@naver.com>
2023-12-18 00:16:09 +0900

    1285_동전 뒤집기

--- src/main/kotlin/boj/greedy/1285_동전 뒤집기.kt
+++ src/main/kotlin/boj/greedy/1285_동전 뒤집기.kt
@@ -19,36 +19,39 @@ fun main() = with(System.`in`.bufferedReader()) {
 
     var mnCnt = Int.MAX_VALUE
     for (b in 0 until 2.0.pow(n).toInt()) {
-        val newGraph = flipRow(n, b, graph)
-        val cnt = countT(n, newGraph)
+        flipRow(n, b, graph)
+        val cnt = countT(n, graph)
         mnCnt = min(mnCnt, cnt)
+        flipRow(n, b, graph)
     }
     println(mnCnt)
 
     close()
 }
 
-private fun flipRow(n: Int, rowBit: Int, graph: MutableList<MutableList<Char>>): MutableList<MutableList<Char>> {
-    val newGraph = graph.map { it.toMutableList() }.toMutableList()
+private fun flipRow(n: Int, rowBit: Int, graph: MutableList<MutableList<Char>>) {
     for (i in 0 until n) {
-        if (rowBit and i == 1) {
-            for (j in newGraph[i].indices) {
-                if (newGraph[i][j] == 'T') {
-                    newGraph[i][j] = 'H'
+        if (rowBit and (1 shl i) != 0) {
+            for (j in graph[i].indices) {
+                if (graph[i][j] == 'T') {
+                    graph[i][j] = 'H'
                 } else {
-                    newGraph[i][j] = 'T'
+                    graph[i][j] = 'T'
                 }
             }
         }
     }
-    return newGraph
 }
 
 private fun countT(n: Int, graph: List<List<Char>>): Int {
     var result = 0
     for (j in 0 until n) {
-        val colList = graph.map { it[j] }
-        val cnt = colList.count { it == 'T' }
+        var cnt = 0
+        graph.forEach { row ->
+            if (row[j] == 'T') {
+                cnt += 1
+            }
+        }
         result += min(n - cnt, cnt)
     }
     return result
