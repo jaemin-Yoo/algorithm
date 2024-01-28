1469b289d01e2c514f2fb323d8a5372974cfa7ea
jaemin-Yoo <iamjm29@gmail.com>
2024-01-28 10:31:43 +0900

    1406_에디터

--- src/main/kotlin/boj/data_structures/1406_에디터.kt
+++ src/main/kotlin/boj/data_structures/1406_에디터.kt
@@ -6,99 +6,40 @@ package boj.data_structures
  * 소요 시간(분): (x)
  */
 
-data class Node(
-    val preIdx: Int?,
-    val nextIdx: Int?,
-    val data: Char?
-)
-
 fun main() = with(System.`in`.bufferedReader()) {
     val s = readLine()
     val m = readLine().toInt()
-    val arr = mutableListOf<Node?>(Node(null, 1, null))
-    for (i in s.indices) {
-        val nextIdx = if (i == s.length - 1) {
-            null
-        } else {
-            i + 2
-        }
-        arr.add(Node(i, nextIdx, s[i]))
+    val s1 = ArrayDeque<Char>()
+    val s2 = ArrayDeque<Char>()
+    for (c in s) {
+        s1.add(c)
     }
-    var cursorIdx = s.length
-    var startIdx = 1
     repeat(m) {
         val row = readLine().split(" ")
-        when (row[0]) {
-            "L" -> cursorIdx = arr[cursorIdx]!!.preIdx ?: return@repeat
-            "D" -> cursorIdx = arr[cursorIdx]!!.nextIdx ?: return@repeat
-            "B" -> {
-                val curNode = arr[cursorIdx] ?: return@repeat
-                if (curNode.preIdx == null) return@repeat
-
-                if (curNode.preIdx == 0) {
-                    startIdx = curNode.nextIdx ?: -1
-                }
-
-                val preNode = arr[curNode.preIdx]!!
-                arr[curNode.preIdx] = Node(preNode.preIdx, curNode.nextIdx, preNode.data)
-
-                if (curNode.nextIdx != null) {
-                    val nextNode = arr[curNode.nextIdx]!!
-                    arr[curNode.nextIdx] = Node(curNode.preIdx, nextNode.nextIdx, nextNode.data)
-                }
-                arr[cursorIdx] = null
-                cursorIdx = curNode.preIdx
+        when (row[0][0]) {
+            'L' -> if (s1.isNotEmpty()) {
+                val c = s1.removeLast()
+                s2.add(c)
             }
-            "P" -> {
-                val c = row[1][0]
-                val curNode = arr[cursorIdx] ?: return@repeat
-
-                if (curNode.preIdx == null && curNode.nextIdx == null) {
-                    arr.add(Node(0, null, c))
-                    arr[cursorIdx] = Node(null, arr.lastIndex, null)
-                    startIdx = arr.lastIndex
-                } else if (curNode.nextIdx == null) {
-                    arr.add(Node(cursorIdx, null, c))
-                    arr[cursorIdx] = Node(curNode.preIdx, arr.lastIndex, curNode.data)
-                } else if (curNode.preIdx == null) {
-                    val nextNode = arr[curNode.nextIdx] ?: return@repeat
-                    arr.add(Node(0, curNode.nextIdx, c))
-                    arr[cursorIdx] = Node(null, arr.lastIndex, null)
-                    arr[curNode.nextIdx] = Node(arr.lastIndex, nextNode.nextIdx, nextNode.data)
-                    startIdx = arr.lastIndex
-                } else {
-                    val nextNode = arr[curNode.nextIdx]!!
-                    arr.add(Node(cursorIdx, curNode.nextIdx, c))
-                    arr[cursorIdx] = Node(curNode.preIdx, arr.lastIndex, curNode.data)
-                    arr[curNode.nextIdx] = Node(arr.lastIndex, nextNode.nextIdx, nextNode.data)
-                }
-                cursorIdx = arr[cursorIdx]!!.nextIdx ?: -1
+            'D' -> if (s2.isNotEmpty()) {
+                val c = s2.removeLast()
+                s1.add(c)
             }
+            'B' -> if (s1.isNotEmpty()) {
+                s1.removeLast()
+            }
+            'P' -> s1.add(row[1][0])
         }
     }
-
+    while (s1.isNotEmpty()) {
+        val c = s1.removeLast()
+        s2.add(c)
+    }
     val result = StringBuilder()
-    var node = arr[startIdx]
-    while (node != null) {
-        result.append(node.data)
-        node = arr[node.nextIdx ?: break]!!
+    while (s2.isNotEmpty()) {
+        val c = s2.removeLast()
+        result.append(c)
     }
     println(result)
     close()
 }
-
-/*
-
-abc
-8
-B
-B
-B
-B
-P q
-P w
-P e
-P r
-
-
- */
