8445d4abd23aeeec072d6801dc30c44a2ad8a825
jaemin-Yoo <iamjm29@gmail.com>
2024-03-10 22:56:05 +0900

    23057_도전 숫자왕

--- src/main/kotlin/boj/hashing/23057_도전 숫자왕.kt
+++ src/main/kotlin/boj/hashing/23057_도전 숫자왕.kt
@@ -6,20 +6,21 @@ package boj.hashing
  * 소요 시간(분): (x)
  */
 
+private val sumList = hashSetOf<Int>()
+
 fun main() = with(System.`in`.bufferedReader()) {
     val n = readLine().toInt()
     val arr = readLine().split(" ").map { it.toInt() }
     val maxSum = arr.sum()
-    val sumList = hashSetOf<Int>()
-    dfs(arr, sumList, 0, 0)
+    dfs(arr, 0, 0)
     println(maxSum - sumList.size + 1)
     close()
 }
 
-private fun dfs(arr: List<Int>, sumList: HashSet<Int>, idx: Int, sum: Int) {
+private fun dfs(arr: List<Int>, idx: Int, sum: Int) {
     sumList.add(sum)
     if (idx == arr.size) return
 
-    dfs(arr, sumList, idx + 1, sum)
-    dfs(arr, sumList, idx + 1, sum + arr[idx])
+    dfs(arr, idx + 1, sum)
+    dfs(arr, idx + 1, sum + arr[idx])
 }
