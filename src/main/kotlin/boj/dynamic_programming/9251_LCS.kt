c47e89d7935baf5fff0427834b6a28331a78f9a7
jaemin-Yoo <iamjm29@gmail.com>
2024-04-19 12:26:13 +0900

    9251_LCS

---
+++ src/main/kotlin/boj/dynamic_programming/9251_LCS.kt
@@ -0,0 +1,27 @@
+package boj.dynamic_programming
+
+/**
+ * 문제 번호: (9251)
+ * 문제 이름: (LCS)
+ * 소요 시간(분): (x)
+ */
+
+import kotlin.math.max
+
+fun main() = System.`in`.bufferedReader().use { br ->
+    val a = br.readLine()
+    val b = br.readLine()
+    val n = a.length
+    val m = b.length
+    val dp = Array(n + 1) { IntArray(m + 1) }
+    for (i in 1 until n + 1) {
+        for (j in 1 until m + 1) {
+            if (a[i - 1] == b[j - 1]) {
+                dp[i][j] = dp[i - 1][j - 1] + 1
+            } else {
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+            }
+        }
+    }
+    println(dp[n][m])
+}
