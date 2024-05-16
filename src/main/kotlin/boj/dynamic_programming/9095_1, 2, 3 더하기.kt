c751a1d94ddc8597b5d706cd5dcabe2431eebef4
jaemin-Yoo <iamjm29@naver.com>
2024-05-16 13:09:22 +0900

    2839_설탕 배달

---
+++ src/main/kotlin/boj/dynamic_programming/9095_1, 2, 3 더하기.kt
@@ -0,0 +1,22 @@
+package boj.dynamic_programming
+
+/**
+ * 문제 번호: (9095)
+ * 문제 이름: (1, 2, 3 더하기)
+ * 소요 시간(분): (x)
+ */
+
+fun main() = System.`in`.bufferedReader().use { br ->
+    val t = br.readLine().toInt()
+    val dp = IntArray(11)
+    dp[1] = 1
+    dp[2] = 2
+    dp[3] = 4
+    for (i in 4 until 11) {
+        dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3]
+    }
+    repeat(t) {
+        val n = br.readLine().toInt()
+        println(dp[n])
+    }
+}
