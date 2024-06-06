30332e61f6e5cc296ae320f8349f7beb55541532
jaemin-Yoo <iamjm29@naver.com>
2024-06-06 11:06:09 +0900

    12865_평범한 배낭

--- src/main/kotlin/boj/dynamic_programming/12865_평범한 배낭.kt
+++ src/main/kotlin/boj/dynamic_programming/12865_평범한 배낭.kt
@@ -10,21 +10,21 @@ import kotlin.math.max
 
 fun main() = System.`in`.bufferedReader().use { br ->
     val (n, k) = br.readLine().split(' ').map { it.toInt() }
-    val dp = Array(n + 1) { IntArray(k + 1) }
-    val w = mutableListOf(0)
-    val v = mutableListOf(0)
-    repeat(n) { i ->
-        val (weight, value) = br.readLine().split(' ').map { it.toInt() }
-        w.add(weight)
-        v.add(value)
+    val ws = mutableListOf(0)
+    val vs = mutableListOf(0)
+    repeat(n) {
+        val (w, v) = br.readLine().split(' ').map { it.toInt() }
+        ws.add(w)
+        vs.add(v)
     }
 
+    val dp = Array(n + 1) { IntArray(k + 1) }
     for (i in 1 until n + 1) {
         for (j in 1 until k + 1) {
-            if (j < w[i]) {
+            if (j < ws[i]) {
                 dp[i][j] = dp[i - 1][j]
             } else {
-                dp[i][j] = max(dp[i - 1][j - w[i]] + v[i], dp[i - 1][j])
+                dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - ws[i]] + vs[i])
             }
         }
     }
