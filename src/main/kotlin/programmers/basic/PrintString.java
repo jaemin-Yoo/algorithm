ac7edf7587dff6b0bb2332aa4ca106f7d8b61814
jaemin-Yoo <iamjm29@naver.com>
2024-06-24 06:00:16 +0900

    문자열 출력하기 (java)

---
+++ src/main/kotlin/programmers/basic/PrintString.java
@@ -0,0 +1,11 @@
+package programmers.basic;
+
+import java.util.Scanner;
+
+public class PrintString {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        System.out.print(a);
+    }
+}
