e8ab5a07f31cc19f1c52ce7569151d78a3f6ff7c
jaemin-Yoo <iamjm29@naver.com>
2024-06-26 21:30:55 +0900

    1157_단어 공부

--- src/main/kotlin/boj/Main.java
+++ src/main/kotlin/boj/Main.java
@@ -4,18 +4,36 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+import java.util.HashMap;
+
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        int c = 1;
-        int v = 6;
-        int answer = 1;
-        while (n > c) {
-            c += v;
-            v += 6;
-            answer++;
+        String s = br.readLine().toUpperCase();
+
+        HashMap<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (map.containsKey(c)) {
+                map.put(c, map.get(c) + 1);
+            } else {
+                map.put(c, 1);
+            }
         }
-        System.out.println(answer);
+
+        int maxCount = 0;
+        Character result = '?';
+
+        for (HashMap.Entry<Character, Integer> entry : map.entrySet()) {
+            Character key = entry.getKey();
+            Integer value = entry.getValue();
+            if (maxCount < value) {
+                maxCount = value;
+                result = key;
+            } else if (maxCount == value) {
+                result = '?';
+            }
+        }
+        System.out.println(result);
     }
 }
