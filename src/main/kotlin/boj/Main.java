04f1b328d0c413d858f5556e35bed84965e486e3
jaemin-Yoo <iamjm29@naver.com>
2024-06-26 20:06:49 +0900

    23971_ZOAC 4

--- src/main/kotlin/boj/Main.java
+++ src/main/kotlin/boj/Main.java
@@ -3,50 +3,21 @@ package boj;
 import java.util.*;
 
 public class Main {
-
-    private static final int[] dx = {1, -1, 0, 0};
-    private static final int[] dy = {0, 0, 1, -1};
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int h = sc.nextInt();
+        int w = sc.nextInt();
         int n = sc.nextInt();
         int m = sc.nextInt();
 
-        int[][] graph = new int[n][m];
-        for (int i = 0; i < n; i++) {
-            String s = sc.next();
-            for (int j = 0; j < m; j++) {
-                graph[i][j] = s.charAt(j) - '0';
-            }
+        int a = 0;
+        for (int i = 0; i < h; i += n + 1) {
+            a++;
         }
-
-        int result = bfs(graph, n, m);
-        System.out.println(result);
-    }
-
-    private static int bfs(int[][] graph, int n, int m) {
-        boolean[][] visited = new boolean[n][m];
-        Queue<int[]> q = new LinkedList<>();
-        q.add(new int[] {0, 0, 1});
-
-        while (!q.isEmpty()) {
-            int[] v = q.poll();
-            int x = v[0];
-            int y = v[1];
-            int dist = v[2];
-            if (x == n - 1 && y == m - 1) {
-                return dist;
-            }
-
-            for (int i = 0; i < 4; i++) {
-                int a = dx[i] + x;
-                int b = dy[i] + y;
-                if ((a >= 0 && a < n) && (b >= 0 && b < m) && graph[a][b] == 1 && !visited[a][b]) {
-                    visited[a][b] = true;
-                    q.add(new int[] {a, b, dist + 1});
-                }
-            }
+        int b = 0;
+        for (int i = 0; i < w; i += m + 1) {
+            b++;
         }
-        return -1;
+        System.out.println(a * b);
     }
 }
