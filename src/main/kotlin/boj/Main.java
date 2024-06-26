0b97fb79c6b10f72a4b43d29f661c02e4ae7905b
jaemin-Yoo <iamjm29@naver.com>
2024-06-26 21:16:51 +0900

    2292_벌집

--- src/main/kotlin/boj/Main.java
+++ src/main/kotlin/boj/Main.java
@@ -1,37 +1,21 @@
 package boj;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int[] arr = new int[3];
-        while (true) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            arr[0] = Integer.parseInt(st.nextToken());
-            arr[1] = Integer.parseInt(st.nextToken());
-            arr[2] = Integer.parseInt(st.nextToken());
-            if (arr[0] == 0 && arr[1] == 0 && arr[2] == 0) {
-                break;
-            }
-            Arrays.sort(arr);
-
-            String answer;
-            int side1 = arr[0];
-            int side2 = arr[1];
-            int side3 = arr[2];
-            if (side3 >= side1 + side2) {
-                answer = "Invalid";
-            } else if (side1 == side2 && side2 == side3) {
-                answer = "Equilateral";
-            } else if (side1 == side2 || side2 == side3) {
-                answer = "Isosceles";
-            } else {
-                answer = "Scalene";
-            }
-            System.out.println(answer);
+        int n = Integer.parseInt(br.readLine());
+        int c = 1;
+        int v = 6;
+        int answer = 1;
+        while (n > c) {
+            c += v;
+            v += 6;
+            answer++;
         }
+        System.out.println(answer);
     }
 }
