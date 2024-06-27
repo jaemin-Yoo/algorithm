b0a9f56e68342e3a1b2fe074211e7e17ed566cad
jaemin-Yoo <iamjm29@naver.com>
2024-06-27 19:52:04 +0900

    8979_올림픽

--- src/main/kotlin/boj/Main.java
+++ src/main/kotlin/boj/Main.java
@@ -8,23 +8,29 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int p = Integer.parseInt(br.readLine());
-        for (int i = 0; i < p; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int t = Integer.parseInt(st.nextToken());
-            ArrayList<Integer> arr = new ArrayList<>();
-            int cnt = 0;
-            for (int j = 0; j < 20; j++) {
-                int h = Integer.parseInt(st.nextToken());
-                arr.add(h);
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
 
-                for (int k = 0; k < j; k++) {
-                    if (arr.get(k) > h) {
-                        cnt++;
-                    }
+        int[][] medals = new int[n + 1][3];
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            int country = Integer.parseInt(st.nextToken());
+            int gold = Integer.parseInt(st.nextToken());
+            int silver = Integer.parseInt(st.nextToken());
+            int bronze = Integer.parseInt(st.nextToken());
+
+            medals[country] = new int[]{gold, silver, bronze};
+        }
+
+        int rank = 1;
+        for (int i = 1; i < n + 1; i++) {
+            if (i != k) {
+                if ((medals[i][0] > medals[k][0]) || (medals[i][0] == medals[k][0] && medals[i][1] > medals[k][1]) || (medals[i][0] == medals[k][0] && medals[i][1] == medals[k][1] && medals[i][2] > medals[k][2])) {
+                    rank++;
                 }
             }
-            System.out.println(t + " " + cnt);
         }
+        System.out.println(rank);
     }
 }
