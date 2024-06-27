a834bb42d8cb1b12a2e8217f31aa5cb4cc6a4ba8
jaemin-Yoo <iamjm29@naver.com>
2024-06-27 20:37:34 +0900

    4659_비밀번호 발음하기

--- src/main/kotlin/boj/Main.java
+++ src/main/kotlin/boj/Main.java
@@ -6,34 +6,60 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
+
+    private final static char[] vowels = {'a', 'e', 'i', 'o', 'u'};
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-
-        int[][] arr = new int[n][2];
-        for (int i = 0; i < n; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int w = Integer.parseInt(st.nextToken());
-            int h = Integer.parseInt(st.nextToken());
-            arr[i][0] = w;
-            arr[i][1] = h;
-        }
-
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < n; i++) {
-            int rank = 1;
-            for (int j = 0; j < n; j++) {
-                if (i != j) {
-                    if (arr[i][0] < arr[j][0] && arr[i][1] < arr[j][1]) {
-                        rank += 1;
-                    }
-                }
+        while (true) {
+            String password = br.readLine();
+            if (password.equals("end")) {
+                break;
             }
-            sb.append(rank);
-            if (i < n - 1) {
-                sb.append(" ");
+
+            if (isValid(password)) {
+                sb.append(String.format("<%s> is acceptable.\n", password));
+            } else {
+                sb.append(String.format("<%s> is not acceptable.\n", password));
             }
         }
         System.out.println(sb);
+    }
+
+    private static boolean isValid(String word) {
+        char prev = ' ';
+        int vowelCnt = 0;
+        int consonantCnt = 0;
+        boolean containsVowel = false;
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            if (c == prev && c != 'e' && c != 'o') {
+                return false;
+            }
+            prev = c;
+
+            boolean isVowel = false;
+            for (char vowel : vowels) {
+                if (c == vowel) {
+                    isVowel = true;
+                    containsVowel = true;
+                    break;
+                }
+            }
+            if (isVowel) {
+                vowelCnt++;
+                consonantCnt = 0;
+            } else {
+                consonantCnt++;
+                vowelCnt = 0;
+            }
+
+            if (vowelCnt == 3 || consonantCnt == 3) {
+                return false;
+            }
+        }
+
+        return containsVowel;
     }
 }
