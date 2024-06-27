0910ebb20f2ca6ede709131914ff0c3916d86709
jaemin-Yoo <iamjm29@naver.com>
2024-06-27 19:33:40 +0900

    10431_줄세우기

--- src/main/kotlin/boj/Main.java
+++ src/main/kotlin/boj/Main.java
@@ -3,16 +3,28 @@ package boj;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
+        int p = Integer.parseInt(br.readLine());
+        for (int i = 0; i < p; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int t = Integer.parseInt(st.nextToken());
+            ArrayList<Integer> arr = new ArrayList<>();
+            int cnt = 0;
+            for (int j = 0; j < 20; j++) {
+                int h = Integer.parseInt(st.nextToken());
+                arr.add(h);
 
-        if (n % 2 == 0) {
-            System.out.println("CY");
-        } else {
-            System.out.println("SK");
+                for (int k = 0; k < j; k++) {
+                    if (arr.get(k) > h) {
+                        cnt++;
+                    }
+                }
+            }
+            System.out.println(t + " " + cnt);
         }
     }
 }
