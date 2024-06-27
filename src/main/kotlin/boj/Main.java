dccc6ee63ee7caee38124405f674c8f3f0015a47
jaemin-Yoo <iamjm29@naver.com>
2024-06-27 15:55:48 +0900

    11723_집합

--- src/main/kotlin/boj/Main.java
+++ src/main/kotlin/boj/Main.java
@@ -3,37 +3,69 @@ package boj;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-
-import java.util.HashMap;
+import java.util.StringTokenizer;
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String s = br.readLine().toUpperCase();
+        int m = Integer.parseInt(br.readLine());
 
-        HashMap<Character, Integer> map = new HashMap<>();
-        for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-            if (map.containsKey(c)) {
-                map.put(c, map.get(c) + 1);
+        StringBuilder sb = new StringBuilder();
+        ArrayList<Integer> arr = new ArrayList<>();
+        for (int i = 0; i < m; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String cmd = st.nextToken();
+            int v;
+            if (cmd.equals("all") || cmd.equals("empty")) {
+                v = -1;
             } else {
-                map.put(c, 1);
+                v = Integer.parseInt(st.nextToken());
+            }
+            switch (cmd) {
+                case "add":
+                    if (!arr.contains(v)) {
+                        arr.add(v);
+                    }
+                    break;
+                case "remove":
+                    arr = remove(arr, v);
+                    break;
+                case "check":
+                    if (arr.contains(v)) {
+                        sb.append("1\n");
+                    } else {
+                        sb.append("0\n");
+                    }
+                    break;
+                case "toggle":
+                    if (arr.contains(v)) {
+                        arr = remove(arr, v);
+                    } else {
+                        arr.add(v);
+                    }
+                    break;
+                case "all":
+                    arr.clear();
+                    for (int j = 1; j < 21; j++) {
+                        arr.add(j);
+                    }
+                    break;
+                case "empty":
+                    arr.clear();
+                    break;
             }
         }
+        System.out.println(sb);
+    }
 
-        int maxCount = 0;
-        Character result = '?';
-
-        for (HashMap.Entry<Character, Integer> entry : map.entrySet()) {
-            Character key = entry.getKey();
-            Integer value = entry.getValue();
-            if (maxCount < value) {
-                maxCount = value;
-                result = key;
-            } else if (maxCount == value) {
-                result = '?';
+    private static ArrayList<Integer> remove(ArrayList<Integer> arr, int v) {
+        ArrayList<Integer> newArr = new ArrayList<>();
+        for (int data : arr) {
+            if (data != v) {
+                newArr.add(data);
             }
         }
-        System.out.println(result);
+        return newArr;
     }
 }
