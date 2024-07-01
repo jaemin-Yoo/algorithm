f631d7b54839ffbc76915f68e3f528b8bef639e7
jaemin-Yoo <iamjm29@naver.com>
2024-07-01 13:31:58 +0900

    1244_스위치 켜고 끄기

--- src/main/kotlin/boj/implementation/1244_스위치 켜고 끄기.kt
+++ src/main/kotlin/boj/implementation/1244_스위치 켜고 끄기.kt
@@ -3,59 +3,57 @@ package boj.implementation
 /**
  * 문제 번호: (1244)
  * 문제 이름: (스위치 켜고 끄기)
- * 소요 시간(분): (27)
+ * 소요 시간(분): (x)
  */
 
 fun main() = System.`in`.bufferedReader().use { br ->
     val n = br.readLine().toInt()
-    val switches = intArrayOf(0) + br.readLine().split(' ').map { it.toInt() }.toIntArray()
-    val m = br.readLine().toInt()
-    repeat(m) {
+    val switches = br.readLine().split(' ').map { it.toInt() }.toIntArray()
+    val t = br.readLine().toInt()
+    repeat(t) {
         val (sex, num) = br.readLine().split(' ').map { it.toInt() }
         if (sex == 1) {
-            man(switches, num, n)
+            operateSwitchByMan(switches, num - 1)
         } else {
-            woman(switches, num, n)
+            operateSwitchByWoman(switches, num - 1)
         }
     }
-    printSwitches(switches, n)
-}
 
-private fun man(switches: IntArray, num: Int, n: Int) {
-    for (i in num until n + 1 step num) {
-        clickSwitch(switches, i)
-    }
-}
-
-private fun woman(switches: IntArray, num: Int, n: Int) {
-    clickSwitch(switches, num)
-
-    var cnt = 1
-    while ((num - cnt) in 1 until n + 1 && (num + cnt) in 1 until n + 1 && switches[num - cnt] == switches[num + cnt]) {
-        clickSwitch(switches, num - cnt)
-        clickSwitch(switches, num + cnt)
-        cnt++
-    }
-}
-
-private fun printSwitches(switches: IntArray, n: Int) {
     val sb = StringBuilder()
     for (i in 1 until n + 1) {
-        sb.append("${switches[i]}")
+        sb.append(switches[i - 1])
+        if (i == n) break
+
         if (i % 20 == 0) {
-            sb.appendLine()
+            sb.append("\n")
         } else {
             sb.append(" ")
         }
     }
-    sb.deleteAt(sb.lastIndex)
     println(sb)
 }
 
-private fun clickSwitch(switches: IntArray, num: Int) {
-    switches[num] = if (switches[num] == 1) {
-        0
-    } else {
-        1
+private fun operateSwitchByMan(switches: IntArray, num: Int) {
+    for (i in num until switches.size step num + 1) {
+        changeSwitchState(switches, i)
     }
+}
+
+private fun operateSwitchByWoman(switches: IntArray, num: Int) {
+    changeSwitchState(switches, num)
+
+    var i = 1
+    while (num - i >= 0 && num + i < switches.size) {
+        if (switches[num - i] == switches[num + i]) {
+            changeSwitchState(switches, num - i)
+            changeSwitchState(switches, num + i)
+        } else {
+            break
+        }
+        i++
+    }
+}
+
+private fun changeSwitchState(switches: IntArray, index: Int) {
+    switches[index] = 1 - switches[index]
 }
