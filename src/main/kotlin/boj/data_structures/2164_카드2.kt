ba126473d1fa3daa229d8ec312c85e0b5cffafb7
jaemin-Yoo <iamjm29@naver.com>
2024-07-05 10:03:54 +0900

    2164_카드2

--- src/main/kotlin/boj/data_structures/2164_카드2.kt
+++ src/main/kotlin/boj/data_structures/2164_카드2.kt
@@ -3,18 +3,20 @@ package boj.data_structures
 /**
  * 문제 번호: (2164)
  * 문제 이름: (카드2)
- * 소요 시간(분): (4)
+ * 소요 시간(분): (8)
  */
 
 fun main() = System.`in`.bufferedReader().use { br ->
     val n = br.readLine().toInt()
-    val deque = ArrayDeque<Int>()
+    val q = ArrayDeque<Int>()
     for (i in 1 until n + 1) {
-        deque.add(i)
+        q.add(i)
     }
-    while (deque.size > 1) {
-        deque.removeFirst()
-        deque.add(deque.removeFirst())
+
+    while (q.size > 1) {
+        q.removeFirst()
+        val a = q.removeFirst()
+        q.add(a)
     }
-    println(deque.removeFirst())
+    println(q.last())
 }
