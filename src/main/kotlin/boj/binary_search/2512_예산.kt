635c93300184fac99e39cc48e25dbb44d7b1c293
jaemin-Yoo <iamjm29@naver.com>
2024-07-08 14:08:40 +0900

    2512_예산

--- src/main/kotlin/boj/binary_search/2512_예산.kt
+++ src/main/kotlin/boj/binary_search/2512_예산.kt
@@ -3,25 +3,32 @@ package boj.binary_search
 /**
  * 문제 번호: (2512)
  * 문제 이름: (예산)
- * 소요 시간(분): (14)
+ * 소요 시간(분): (19)
  */
 
-fun main() = with(System.`in`.bufferedReader()) {
-    val n = readLine().toInt()
-    val arr = readLine().split(" ").map { it.toInt() }.sorted()
-    var m = readLine().toInt()
-    var x = m / n
-    for (i in 0 until n) {
-        if (x >= arr[n - 1]) {
-            x = arr[n - 1]
-            break
-        }
+import kotlin.math.min
 
-        if (x > arr[i]) {
-            m -= arr[i]
-            x = m / (n - i - 1)
+fun main() = System.`in`.bufferedReader().use { br ->
+    val n = br.readLine().toInt()
+    val arr = br.readLine().split(' ').map { it.toInt() }.toIntArray()
+    val m = br.readLine().toInt()
+    val result = binarySearch(arr, n, m)
+    println(result)
+}
+
+private fun binarySearch(arr: IntArray, n: Int, m: Int): Int {
+    var start = m / n
+    var end = arr.max()
+    var result = arr.sum()
+    while (start <= end) {
+        val mid = (start + end) / 2
+        val newArr = arr.map { min(it, mid) }
+        if (newArr.sum() <= m) {
+            start = mid + 1
+            result = newArr.max()
+        } else {
+            end = mid - 1
         }
     }
-    println(x)
-    close()
+    return result
 }
