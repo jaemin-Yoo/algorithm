f32e796c7fb01bf988a546ae147d8346198c09ad
jaemin-Yoo <iamjm29@naver.com>
2024-07-13 17:46:02 +0900

    2607_비슷한 단어

--- src/main/kotlin/boj/string/2607_비슷한 단어.kt
+++ src/main/kotlin/boj/string/2607_비슷한 단어.kt
@@ -8,25 +8,53 @@ package boj.string
 
 import kotlin.math.abs
 
-fun main(args: Array<String>) = with(System.`in`.bufferedReader()) {
-    val n = readLine().toInt()
-    val word = readLine().toMutableList()
+fun main() = System.`in`.bufferedReader().use { br ->
+    val n = br.readLine().toInt()
+    val word = br.readLine()
     var cnt = 0
     repeat(n - 1) {
-        val s = readLine().toMutableList()
-        val wordSize = word.size
-        val sSize = s.size
-        if (abs(wordSize - sSize) > 1) return@repeat
-
-        word.forEach { c ->
-            s.remove(c)
-        }
-
-        if ((sSize > wordSize && s.size == 1) || (wordSize > sSize && s.isEmpty()) || (wordSize == sSize && s.size <= 1)) {
-            cnt++
-        }
+        val s = br.readLine()
+        val map = getCountMap(word)
+        if (abs(word.length - s.length) <= 1 && areSimilar(map, s)) cnt++
     }
     println(cnt)
+}
 
-    close()
+private fun getCountMap(word: String): HashMap<Char, Int> {
+    val map = HashMap<Char, Int>()
+    word.forEach { c ->
+        if (map.containsKey(c)) {
+            map[c] = map[c]!! + 1
+        } else {
+            map[c] = 1
+        }
+    }
+    return map
+}
+
+private fun areSimilar(countMap: HashMap<Char, Int>, s: String): Boolean {
+    var diffCount = 0
+    s.forEach { c ->
+        if (countMap.containsKey(c)) {
+            countMap[c] = countMap[c]!! - 1
+        } else {
+            diffCount++
+        }
+    }
+
+    if (diffCount > 1) return false
+
+    var oneCnt = 0
+    var minusOneCnt = 0
+    countMap.forEach { (_, v) ->
+        if (abs(v) > 1) {
+            return false
+        } else if (v == 1) {
+            oneCnt++
+        } else if (v == -1) {
+            minusOneCnt++
+        }
+    }
+
+    return (oneCnt <= 1 && minusOneCnt == 0) || (diffCount == 0 && oneCnt <= 1 && minusOneCnt <= 1)
 }
