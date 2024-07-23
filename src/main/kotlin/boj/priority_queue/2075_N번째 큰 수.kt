0369540c4d776a8ddb5a98523b1c677f927e36b2
jaemin-Yoo <iamjm29@naver.com>
2024-07-23 12:25:54 +0900

    2075_N번째 큰 수

--- src/main/kotlin/boj/priority_queue/2075_N번째 큰 수.kt
+++ src/main/kotlin/boj/priority_queue/2075_N번째 큰 수.kt
@@ -3,16 +3,21 @@ package boj.priority_queue
 /**
  * 문제 번호: (2075)
  * 문제 이름: (N번째 큰 수)
- * 소요 시간(분): (3)
+ * 소요 시간(분): (4)
  */
+
+import java.util.PriorityQueue
+import java.util.Collections
 
 fun main() = System.`in`.bufferedReader().use { br ->
     val n = br.readLine().toInt()
-    val lst = mutableListOf<Int>()
+    val pq = PriorityQueue<Int>(Collections.reverseOrder())
     repeat(n) {
-        val row = br.readLine().split(' ').map { it.toInt() }
-        lst.addAll(row)
+        br.readLine().split(' ').map { pq.add(it.toInt()) }
     }
-    lst.sortDescending()
-    println(lst[n - 1])
+    var result = 0
+    repeat(n) {
+        result = pq.poll()
+    }
+    print(result)
 }
