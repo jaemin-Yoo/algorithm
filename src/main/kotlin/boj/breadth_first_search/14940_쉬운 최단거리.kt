39a47c7900cd7490d8b8db85ec0407250a65d116
jaemin-Yoo <iamjm29@naver.com>
2024-07-28 22:12:05 +0900

    14940_쉬운 최단거리

--- src/main/kotlin/boj/breadth_first_search/14940_쉬운 최단거리.kt
+++ src/main/kotlin/boj/breadth_first_search/14940_쉬운 최단거리.kt
@@ -1,81 +1,65 @@
-package boj.breadth_first_search.쉬운_최단거리14940
+package boj.breadth_first_search
 
 /**
  * 문제 번호: (14940)
  * 문제 이름: (쉬운 최단거리)
- * 소요 시간(분): (15)
+ * 소요 시간(분): (x)
  */
-
-import java.util.Queue
-import java.util.LinkedList
 
 private val dx = listOf(1, -1, 0, 0)
 private val dy = listOf(0, 0, 1, -1)
 
-data class Position(
-    val x: Int,
-    val y: Int
-)
-
-data class Node(
-    val pos: Position,
-    val dist: Int
-)
-
-fun main() = with(System.`in`.bufferedReader()) {
-    val (n, m) = readLine().split(" ").map { it.toInt() }
-    val graph = mutableListOf<List<Int>>()
-    repeat(n) {
-        val row = readLine().split(" ").map { it.toInt() }
-        graph.add(row)
+fun main() = System.`in`.bufferedReader().use { br ->
+    val (n, m) = br.readLine().split(' ').map { it.toInt() }
+    val graph = Array(n) { IntArray(m) }
+    repeat(n) { i ->
+        val row = br.readLine().split(' ').map { it.toInt() }.toIntArray()
+        graph[i] = row
     }
-    val start = getStartPosition(graph, n, m)
-    val newGraph = bfs(graph, start, n, m)
-    newGraph.forEach {
+
+    val (x, y) = getStartPosition(graph, n, m)
+    val result = bfs(graph, n, m, x, y)
+    result.forEach {
         println(it.joinToString(" "))
     }
-    close()
 }
 
-private fun bfs(graph: MutableList<List<Int>>, start: Position, n: Int, m: Int): Array<IntArray> {
-    val q: Queue<Node> = LinkedList()
-    q.add(Node(start, 0))
-
-    val newGraph = Array(n) { IntArray(m) { -1 } }
-    for (i in 0 until n) {
-        for (j in 0 until m) {
-            if (graph[i][j] == 0) {
-                newGraph[i][j] = 0
-            }
-        }
-    }
-
-    newGraph[start.x][start.y] = 0
-    while (q.isNotEmpty()) {
-        val (pos, dist) = q.poll()
-        val (x, y) = pos
-        for (i in 0 until 4) {
-            val a = x + dx[i]
-            val b = y + dy[i]
-            if (a in 0 until n && b in 0 until m && newGraph[a][b] == -1 && graph[a][b] == 1) {
-                val newDist = dist + 1
-                newGraph[a][b] = newDist
-                q.add(Node(Position(a, b), newDist))
-            }
-        }
-    }
-
-    return newGraph
-}
-
-private fun getStartPosition(graph: MutableList<List<Int>>, n: Int, m: Int): Position {
+private fun getStartPosition(graph: Array<IntArray>, n: Int, m: Int): Pair<Int, Int> {
     for (i in 0 until n) {
         for (j in 0 until m) {
             if (graph[i][j] == 2) {
-                return Position(i, j)
+                return i to j
             }
         }
     }
 
-    throw Exception()
+    return -1 to -1
+}
+
+private fun bfs(graph: Array<IntArray>, n: Int, m: Int, sx: Int, sy: Int): Array<IntArray> {
+    val visited = Array(n) { IntArray(m) { -1 } }
+    for (i in 0 until n) {
+        for (j in 0 until m) {
+            if (graph[i][j] == 0) {
+                visited[i][j] = 0
+            }
+        }
+    }
+
+    val q = ArrayDeque<IntArray>()
+    q.add(intArrayOf(sx, sy))
+    visited[sx][sy] = 0
+
+    while (q.isNotEmpty()) {
+        val (x, y) = q.removeFirst()
+        for (i in 0 until 4) {
+            val a = x + dx[i]
+            val b = y + dy[i]
+            if (a in 0 until n && b in 0 until m && visited[a][b] == -1) {
+                visited[a][b] = visited[x][y] + 1
+                q.add(intArrayOf(a, b))
+            }
+        }
+    }
+    return visited
 }
