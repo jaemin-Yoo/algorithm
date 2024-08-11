1f4cdf903aa8fa4a842626c638e5e2f44b396c33
jaemin-Yoo <iamjm29@naver.com>
2024-08-11 15:48:09 +0900

    3273_두 수의 합

--- src/main/kotlin/boj/two_pointer/3273_두 수의 합.kt
+++ src/main/kotlin/boj/two_pointer/3273_두 수의 합.kt
@@ -3,25 +3,25 @@ package boj.two_pointer
 /**
  * 문제 번호: (3273)
  * 문제 이름: (두 수의 합)
- * 소요 시간(분): (8)
+ * 소요 시간(분): (3)
  */
 
 fun main() = System.`in`.bufferedReader().use { br ->
     val n = br.readLine().toInt()
     val arr = br.readLine().split(' ').map { it.toInt() }.sorted()
     val x = br.readLine().toInt()
-    var start = 0
-    var end = arr.lastIndex
+    var p1 = 0
+    var p2 = n - 1
     var cnt = 0
-    while (start < end) {
-        if (arr[start] + arr[end] > x) {
-            end--
+    while (p1 < p2) {
+        if (arr[p1] + arr[p2] < x) {
+            p1++
+        } else if (arr[p1] + arr[p2] > x) {
+            p2--
         } else {
-            if (arr[start] + arr[end] == x) {
-                cnt++
-            }
-            start++
+            cnt++
+            p2--
         }
     }
-    println(cnt)
+    print(cnt)
 }
