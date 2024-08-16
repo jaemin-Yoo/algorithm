d5cc063ffb9474fcbcee7024a239ed650cad1f00
jaemin-Yoo <iamjm29@naver.com>
2024-08-16 11:10:29 +0900

    11501_주식

--- src/main/kotlin/boj/greedy/11501_주식.kt
+++ src/main/kotlin/boj/greedy/11501_주식.kt
@@ -3,25 +3,23 @@ package boj.greedy
 /**
  * 문제 번호: (11501)
  * 문제 이름: (주식)
- * 소요 시간(분): (6)
+ * 소요 시간(분): (13)
  */
 
 fun main() = System.`in`.bufferedReader().use { br ->
     val t = br.readLine().toInt()
-    val sb = StringBuilder()
     repeat(t) {
         val n = br.readLine().toInt()
         val arr = br.readLine().split(' ').map { it.toInt() }
-        var maxValue = 0
         var result = 0L
-        for (i in n - 1 downTo 0) {
-            if (maxValue < arr[i]) {
+        var maxValue = arr[n - 1]
+        for (i in n - 2 downTo 0) {
+            if (arr[i] >= maxValue) {
                 maxValue = arr[i]
             } else {
                 result += maxValue - arr[i]
             }
         }
-        sb.appendLine(result)
+        println(result)
     }
-    print(sb)
 }
